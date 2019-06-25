eb350306a743d95145f96baa2971512062691e18
Papabyte <bencreze@yahoo.fr>
2019-06-25 14:55:33 +0200

    initial

---
+++ conf.js
@@ -0,0 +1,12 @@
+exports.bServeAsHub = false;
+exports.bLight = true;
+exports.bSingleAddress = true;
+
+exports.WS_PROTOCOL = "ws://";
+exports.hub = 'devnet.papabyte.com:6611';
+exports.deviceName = 'paid-access-API';
+exports.permanent_pairing_secret = '0000';
+exports.control_addresses = [''];
+
+
+console.log('finished paid-access-API conf');
