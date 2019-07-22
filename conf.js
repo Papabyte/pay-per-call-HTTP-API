17df3c7f6fc91526afad48c29b654b835ffe102d
Papabyte <bencreze@yahoo.fr>
2019-07-22 14:31:25 +0200

    initial

---
+++ conf.js
@@ -0,0 +1,20 @@
+exports.bServeAsHub = false;
+exports.bLight = true;
+exports.bSingleAddress = true;
+
+exports.WS_PROTOCOL = "ws://";
+exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
+exports.deviceName = 'AA-channel-lib';
+exports.permanent_pairing_secret = '0000';
+exports.control_addresses = [''];
+
+exports.aa_version = 1;
+
+exports.isHighAvaibilityNode =  false;
+
+exports.enabledComLayers = ['http','obyte-messenger'];
+
+exports.isHttpServer = true;
+exports.httpDefaultPort = 6800;
+
+console.log('paid access API conf');
